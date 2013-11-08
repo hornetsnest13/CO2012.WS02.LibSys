6e19693a234f67377df6eaa54990fd0bb9eaed73
Declan Williams <declanb.williams@me.com>
2013-11-08 23:36:29 +0000

    Added additional import statements to Account to remove errors

--- CO2012_ LibrarySystem/src/librarySystem/Account.java
+++ CO2012_ LibrarySystem/src/librarySystem/Account.java
@@ -1,6 +1,15 @@
 package librarySystem;
 
+import java.util.Date;
+import java.util.Set;
+import java.util.HashSet;;
+
 public class Account {
+	private int id;
+	private Set<UserItem> history;
+	private Date opened;
+	private AccountState state;
+	
 	public Account(){
 		
 	}
